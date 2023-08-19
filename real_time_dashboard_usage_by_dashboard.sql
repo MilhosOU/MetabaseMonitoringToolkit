6228dcea79089f25aa7edfe2330651886e4ef40d
Samane <samane.shahmohamadi@gmail.com>
2023-08-19 18:40:49 +0330

    Update real_time_dashboard_usage_by_dashboard.sql
    change "_" to "." in column names

--- real_time_dashboard_usage_by_dashboard.sql
+++ real_time_dashboard_usage_by_dashboard.sql
@@ -1,10 +1,10 @@
 select 
-  public.report_dashboard.name as dashboard,
+  report_dashboard.name as dashboard,
   count(*)
-from public.view_log
-inner join public.report_dashboard on (public.report_dashboard.id = public.view_log_model_id)
-inner join public.core_user on (public.view_log_user_id = public.core_user_id) 
-where public.view_log.model = 'dashboard'
-and public.view_log.timestamp > current_date - 2
+from metabase.view_log
+inner join metabase.report_dashboard on (metabase.report_dashboard.id = metabase.view_log.model_id)
+inner join metabase.core_user on (metabase.view_log.user_id = metabase.core_user.id) 
+where metabase.view_log.model = 'dashboard'
+and metabase.view_log.timestamp > current_date - 2
 group by 1
 order by 2 desc
